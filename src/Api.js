ce7484064b64ffc6d34893d2d7660a2b4a82ad1e
woobinslave <mun3582w@naver.com>
2020-11-20 13:06:58 +0900

    proxy추가

--- src/Api.js
+++ src/Api.js
@@ -3,16 +3,16 @@ import axios from "axios";
 export const getMovies = async (PageNumber) => {
   const movies = await axios
     .get(
-      `https://cors-anywhere.herokuapp.com/https://yts.mx/api/v2/list_movies.json?limit=50&page=${Number(
-        PageNumber
-      )}`,
-      {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          origin: "",
-          "x-requested-with": "",
-        },
-      }
+      `/list_movies.json?limit=50&page=${Number(PageNumber)}`
+      // https://cors-anywhere.herokuapp.com/
+      // ,
+      // {
+      //   headers: {
+      //     "Access-Control-Allow-Origin": "*",
+      //     origin: "",
+      //     "x-requested-with": "XMLHttpRequest",
+      //   },
+      // }
     )
     .then((res) => res.data.data.movies);
   return movies;
@@ -21,14 +21,15 @@ export const getMovies = async (PageNumber) => {
 export const getDetail = async (movieID) => {
   const details = await axios
     .get(
-      `https://cors-anywhere.herokuapp.com/https://yts.mx/api/v2/movie_details.json?movie_id=${movieID}`,
-      {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          origin: "",
-          "x-requested-with": "",
-        },
-      }
+      `https://yts.mx/api/v2/movie_details.json?movie_id=${movieID}`
+      // https://cors-anywhere.herokuapp.com/
+      // , {
+      //   headers: {
+      //     "Access-Control-Allow-Origin": "*",
+      //     origin: "",
+      //     "x-requested-with": "",
+      //   },
+      // }
     )
     .then((res) => res.data.data.movie);
   return details;
